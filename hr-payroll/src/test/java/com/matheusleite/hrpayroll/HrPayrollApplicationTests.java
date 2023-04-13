792218f5e6bb9787da1c894569a315171f783521
Matheus Leite <maatheus2710@gmail.com>
2023-04-12 22:20:40 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/matheusleite/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusleite.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
